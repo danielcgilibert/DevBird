f8efbe565b7221460cccb967a14185eaf2e619b9
Daniel <danielcgilibert@gmail.com>
2023-08-30 16:00:33 +0200

    chore: add tailwind conf

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["custom/next"],
-};
+  extends: ['next/babel', 'custom/next'],
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button, Header } from 'ui'
-
 export default function Page(): JSX.Element {
   return (
     <>
-      <Header text='Webasdsad' />
-      <Button />
+      <button className='text-4xl'>hola</button>
     </>
   )
 }
